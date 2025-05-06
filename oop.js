5052e40524930a862334f4edd67dc8393aedeca2
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-05-06 23:00:53 +0530

    add info

--- oop.js
+++ oop.js
@@ -23,6 +23,21 @@ class Library {
   }
 }
 
+class Book {
+  constructor(name, isbn, price, author) {
+    this.name = name; // kitaabe ka naam
+    this.isbn = isbn; // kitaabe ka ISBN number
+    this.price = price; // kitaabe ka price
+    this.author = author; // kitaabe ka author
+  }
+
+  info() {
+    console.log(
+      `Book Name: ${this.name} is written by ${this.author} and is available at amazon at a price range of ${this.price}.`
+    );
+  }
+}
+
 let jhasiLibrary = new Library();
 jhasiLibrary.addbook("Harry Potter"); // kitaabe add karne ka function
 jhasiLibrary.addbook("The Alchemist"); // kitaabe add karne ka function
@@ -30,3 +45,8 @@ jhasiLibrary.addbook("The Da Vinci Code"); // kitaabe add karne ka function
 jhasiLibrary.addbook("The Hobbit"); // kitaabe add karne ka function
 jhasiLibrary.addbook("The Lord of the Rings"); // kitaabe add karne ka function
 jhasiLibrary.listAllBooks(); // kitaabe list karne ka function
+
+let b1 = new Book("Harry Potter", "1234567890", 500, "F. Scott Fitzgerald"); // kitaabe ka object
+let b2 = new Book("The Alchemist", "0987654321", 300, "Paulo Coelho"); // kitaabe ka object
+
+b1.info(); // kitaabe ka info function
