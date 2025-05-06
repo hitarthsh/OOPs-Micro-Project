e7ef5ec9e828ea651a2587e69bdceb1cc3795f98
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-05-06 22:36:45 +0530

    add code

--- oop.js
+++ oop.js
@@ -1,7 +1,15 @@
+// class Library {
+// add kitaabe
+// kitaabe issue
+// kitaabe return
+// kitaabe padhi
+// list of all books
+// }
+
 class Library {
-  // add kitaabe
-  // kitaabe issue
-  // kitaabe return
-  // kitaabe padhi
-  // list of all books
+  constructor() {
+    this.books = []; // saari books ka record yaha par hai
+  }
 }
+
+let jhasiLibrary = new Library();
